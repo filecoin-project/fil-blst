a11ce89da068e1250d8fcd601923fa9fc7d74aed
nemo <nemo@protocol.ai>
2020-09-02 15:45:11 -0400

    Update build.rs (provided by simonatsn)

--- rust/build.rs
+++ rust/build.rs
@@ -1,101 +1,115 @@
-extern crate bindgen;
 extern crate cc;
 
 use std::env;
 use std::path::Path;
 use std::path::PathBuf;
-use std::process::Command;
+
+#[cfg(all(target_env = "msvc", target_arch = "x86_64"))]
+fn assembly(file_vec: &mut Vec<PathBuf>, base_dir: &str) {
+    let files = glob::glob(&(base_dir.to_owned() + "win64/*-x86_64.asm"))
+        .expect("disaster");
+    for file in files {
+        file_vec.push(file.unwrap());
+    }
+}
+
+#[cfg(all(target_env = "msvc", target_arch = "aarch64"))]
+fn assembly(file_vec: &mut Vec<PathBuf>, base_dir: &str) {
+    let files = glob::glob(&(base_dir.to_owned() + "win64/*-armv8.asm"))
+        .expect("disaster");
+    for file in files {
+        file_vec.push(file.unwrap());
+    }
+}
+
+#[cfg(all(target_pointer_width = "64", not(target_env = "msvc")))]
+fn assembly(file_vec: &mut Vec<PathBuf>, base_dir: &str) {
+    file_vec.push(Path::new(base_dir).join("assembly.S"))
+}
+
+#[cfg(target_arch = "x86_64")]
+fn is_adx() -> bool {
+    use std::arch::x86_64::*;
+    let mut id = unsafe { __cpuid(0) };
+    if id.eax >= 7 {
+        id = unsafe { __cpuid_count(7, 0) };
+        return (id.ebx & 1 << 19) != 0;
+    }
+    false
+}
+#[cfg(not(target_arch = "x86_64"))]
+fn is_adx() -> bool {
+    false
+}
 
 fn main() {
-    // TODO - could ls directory and find all files
-    let asm_to_build = [
-        "add_mod_256-x86_64",
-        "add_mod_384-x86_64",
-        "mulq_mont_256-x86_64",
-        "mulx_mont_256-x86_64",
-        "sha256-x86_64",
-        "add_mod_384x384-x86_64",
-        "inverse_mod_384-x86_64",
-        "mulq_mont_384-x86_64",
-        "mulx_mont_384-x86_64",
-    ];
+    /*
+     * Use pre-built libblst.a if there is one. This is primarily
+     * for trouble-shooting purposes. Idea is that libblst.a can be
+     * compiled with flags independent from cargo defaults, e.g.
+     * '../../build.sh -O1 ...'.
+     */
+    if Path::new("libblst.a").exists() {
+        println!("cargo:rustc-link-search=.");
+        println!("cargo:rustc-link-lib=blst");
+        return;
+    }
 
     let mut file_vec = Vec::new();
-    let mut cpp_file_vec = Vec::new();
 
-    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let _out_dir = env::var_os("OUT_DIR").unwrap();
 
-    let blst_base_dir = match env::var("BLST_SRC_DIR") {
+    let lotus_blst_base_dir = match env::var("LOTUS_BLST_SRC_DIR") {
         Ok(val) => val,
         Err(_) => {
-            if Path::new("blst").exists() {
-                "blst".to_string()
+            if Path::new("lotus-blst").exists() {
+                "lotus-blst".to_string()
             } else {
-                "../blst".to_string()
+                "..".to_string()
             }
         }
     };
-    println!("Using blst source directory {:?}", blst_base_dir);
+    let blst_base_dir = lotus_blst_base_dir.clone() + "/blst";
+    println!("Using lotus-blst source directory {:?}", lotus_blst_base_dir);
+    println!("Using       blst source directory {:?}", blst_base_dir);
 
-    let perl_src_dir = blst_base_dir.clone() + "/src/asm/";
     let c_src_dir = blst_base_dir.clone() + "/src/";
+    let build_dir = blst_base_dir.clone() + "/build/";
     let binding_src_dir = blst_base_dir + "/bindings/";
-    //let blst_src_dir = blst_base_dir + "src/";
-
-    for a in asm_to_build.iter() {
-        let dest_path = Path::new(&out_dir).join(a).with_extension("s");
-        let src_path = Path::new(&perl_src_dir).join(a).with_extension("pl");
-
-        Command::new(&src_path)
-            .args(&[">", dest_path.to_str().unwrap()])
-            .status()
-            .unwrap();
-
-        file_vec.push(dest_path);
-    }
+    let lotus_blst_src_dir = lotus_blst_base_dir.clone() + "/src/";
 
     file_vec.push(Path::new(&c_src_dir).join("server.c"));
-    cpp_file_vec.push(Path::new("../src/").join("lotus_blst.cpp"));
-    cpp_file_vec.push(Path::new("../src/").join("thread_pool.cpp"));
+    assembly(&mut file_vec, &build_dir);
+
+    let mut cpp_file_vec = Vec::new();
+    cpp_file_vec.push(Path::new(&lotus_blst_src_dir).join("lotus_blst.cpp"));
+    cpp_file_vec.push(Path::new(&lotus_blst_src_dir).join("thread_pool.cpp"));
 
     // Set CC environment variable to choose alternative C compiler.
     // Optimization level depends on whether or not --release is passed
-    // or implied. If default "release" level of 3 is deemed unsuitable,
-    // modify 'opt-level' in [profile.release] in Cargo.toml.
+    // or implied.
+    let mut cc = cc::Build::new();
+    if is_adx() {
+        cc.define("__ADX__", None);
+    }
+    cc.flag_if_supported("-mno-avx") // avoid costly transitions
+        .flag_if_supported("-Wno-unused-command-line-argument");
+    if !cfg!(debug_assertions) {
+        cc.opt_level(3); // Must be consistent with Go build
+    }
+    cc.files(&file_vec).compile("libblst.a");
 
-    // In order to avoid multiple definitions of the blst symbols in Lotus
-    // the flags here must batch what is in lotus_blst.go and blst.go.
-    cc::Build::new()
-        .flag("-O3")
-        .flag("-D__ADX__")
-        .flag_if_supported("-mno-avx") // avoid costly transitions
-        .files(&file_vec)
-        .compile("libblst.a");
-
-    // native is faster but causes machine to machine incompatibility
-    // .flag("-march=native")
-    cc::Build::new()
-        .flag("-O3")
-        .flag("-D__ADX__")
-        .flag_if_supported("-mno-avx") // avoid costly transitions
-        .flag_if_supported("-Wno-unused-command-line-argument")
-        .cpp(true)
+    // Build lotus-blst
+    let mut cpp = cc::Build::new();
+    if is_adx() {
+        cpp.define("__ADX__", None);
+    }
+    cpp.flag_if_supported("-mno-avx"); // avoid costly transitions
+    if !cfg!(debug_assertions) {
+        cpp.opt_level(3); // Must be consistent with Go build
+    }
+    cpp.cpp(true)
         .include(&binding_src_dir)
-        //.include(&c_src_dir)
         .files(&cpp_file_vec)
         .compile("liblotusblst.a");
-
-    let bindings = bindgen::Builder::default()
-        .header(binding_src_dir + "blst.h")
-        .opaque_type("blst_pairing")
-        .size_t_is_usize(true)
-        .rustified_enum("BLST_ERROR")
-        .generate()
-        .expect("Unable to generate bindings");
-
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
 }
