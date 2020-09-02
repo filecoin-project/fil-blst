994cf37aa5aa5c97b4c8ab6ef050ca26620bf5b9
Volker Mische <volker.mische@gmail.com>
2020-09-02 23:57:06 +0200

    fix: add bindgen generation back
    The previous commit removed the bindings.rs generation.

--- rust/build.rs
+++ rust/build.rs
@@ -112,4 +112,18 @@ fn main() {
         .include(&binding_src_dir)
         .files(&cpp_file_vec)
         .compile("liblotusblst.a");
+
+    let bindings = bindgen::Builder::default()
+        .header(binding_src_dir + "blst.h")
+        .opaque_type("blst_pairing")
+        .size_t_is_usize(true)
+        .rustified_enum("BLST_ERROR")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
